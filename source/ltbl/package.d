42e7255eb80eb145f270331463e3be352a2de86f
Devin R <devin.ridgway@gmail.com>
2015-09-04 14:13:30 -0700

    added package.d for better module naming

---
+++ source/ltbl/package.d
@@ -0,0 +1,30 @@
+/*
+ltbl.d - A lighting framework for the D programming language and DSFML.
+Copyright (C) 2015 Devin Ridgway
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+module ltbl;
+
+public import ltbl.math;
+public import ltbl.lighting.lightdirectionemission;
+public import ltbl.lighting.lightpointemission;
+public import ltbl.lighting.lightshape;
+public import ltbl.lighting.lightsystem;
+public import ltbl.quadtree.dynamicquadtree;
+public import ltbl.quadtree.quadtree;
+public import ltbl.quadtree.quadtreenode;
+public import ltbl.quadtree.quadtreeoccupant;
+public import ltbl.quadtree.staticquadtree;
